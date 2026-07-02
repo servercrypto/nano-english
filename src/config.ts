dfa1fa685bbeb41a50fa9e58272751fe3835926b
servercrypto <servercrypto777@gmail.com>
2026-07-02 15:41:04 +0300

    fix: point NEXT_PUBLIC_URL to actual production domain

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
-// use NODE_ENV to not have to change config based on where it's deployed
+// Предотвращение конфликтов маршрутизации деплоя
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://onchain-app-template.vercel.app';
-// Add your API KEY from the Coinbase Developer Portal
+    : 'https://nano-english-kids.vercel.app';
+
+// API Ключи авторизации девелоперского пула Coinbase CDP
 export const NEXT_PUBLIC_CDP_API_KEY = process.env.NEXT_PUBLIC_CDP_API_KEY;
 export const NEXT_PUBLIC_WC_PROJECT_ID = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
